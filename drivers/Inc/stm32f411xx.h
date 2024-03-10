25b88fe5500e7e33b959c1a29da9288104aa2374
hung-hv <viethung170230@gmail.com>
2024-03-10 13:27:24 +0700

    update .h

--- drivers/Inc/stm32f411xx.h
+++ drivers/Inc/stm32f411xx.h
@@ -8,6 +8,27 @@
 #ifndef INC_STM32F411XX_H_
 #define INC_STM32F411XX_H_
 
+/* system memories*/
+#define MCU_BASEADDR		0x08000000
+#define SRAM_BASEADDR		MCU_BASEADDR
+//#define
 
+/* System bus define */
+#define
+#define APB1_BASEADDR		0x40000000
+#define APB2_BASEADDR		0x40010000
+#define AHB1_BASEADDR		0x40020000
+#define AHB2_BASEADDR		0x50000000
+
+/*
+ * Base address of all peripherals hanging on APB1 bus
+ */
+#define GPIOA_BASEADDR		(APB1_BASEADDR + 0x0000)
+#define GPIOB_BASEADDR		(APB1_BASEADDR + 0x0400)
+#define GPIOC_BASEADDR		(APB1_BASEADDR + 0x0800)
+
+/*
+ * Base address of all peripherals hanging on APB2 bus
+ */
 
 #endif /* INC_STM32F411XX_H_ */
